1e602fc91bc069fb0a9bfb8552f797f2118ba4ed
unknown <smadithya@yahoo.com>
2020-08-05 20:06:03 +0530

    try

--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -1,6 +1,7 @@
 
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,7 +143,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'adithyaharishsm@gmail.com'
 EMAIL_HOST_PASSWORD = 'yaqzrryxltykmztc'
 
-
+django_heroku.settings(locals())
 
 
 
