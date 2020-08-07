fab2cfe7a78cd48610029fce87789fcd34eaae3b
unknown <smadithya@yahoo.com>
2020-08-07 20:43:22 +0530

    try

--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -117,7 +117,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK ='bootstrap4'
 
