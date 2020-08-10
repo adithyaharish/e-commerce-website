5b1c27e61d5cfc096cbcada68f64e2eb6bde9019
unknown <smadithya@yahoo.com>
2020-08-10 17:41:08 +0530

    try

--- djangonautic/settings.py
+++ djangonautic/settings.py
@@ -144,8 +144,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'adithyaharishsm@gmail.com'
-EMAIL_HOST_PASSWORD = 'yaqzrryxltykmztc'
+EMAIL_HOST_USER = '*************@gmail.com'
+EMAIL_HOST_PASSWORD = '*****************'
 
 
 django_heroku.settings(locals())
